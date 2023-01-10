225fae56b25c6fd34dbe47c046c01921d22caa6e
Kihyun92 <okkihyun1@gmail.com>
2023-01-10 23:23:27 +0900

    📦 add next-mdx-remote

--- components/navigator.tsx
+++ components/navigator.tsx
@@ -2,14 +2,14 @@ import Link from 'next/link';
 
 export const Navigator: React.FC = () => {
   return (
-    <div className="px-10 py-12 bg-green-100">
+    <div className="px-10 py-8 bg-green-100">
       <nav className="flex items-center justify-between mx-auto">
         <div>
           <Link href="/">kihyuns blog</Link>
         </div>
         <ol className="flex flex-row">
-          <li><Link href="/wiki">Wiki</Link></li>
-          <li><Link href="/me">Me</Link></li>
+          <li className="px-5 py-2"><Link href="/wiki">Wiki</Link></li>
+          <li className="px-5 py-2"><Link href="/me">Me</Link></li>
         </ol>
         <div>
           <button>☾</button>
